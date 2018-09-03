2c386374f6978329c2c567caa896a12ba95efc9d
Spencer Fern <USpenSt@PEROOT.COM>
2018-09-03 14:04:03 +0530

    CircleCI configuration

--- api/Routes.js
+++ api/Routes.js
@@ -1,22 +1,7 @@
 const express = require("express");
 var routes = express.Router();
 
-var drugRoutes = require("./Routes/Drug.Route");
-var empRoutes = require("./Routers/Emp.Route");
-var issueRoutes = require("./Routers/Issue.Route");
-var paymentRoutes = require("./Routers/Payment.Route");
-var issuedDrugsRoutes = require("./Routes/IssuedDrugs.Route");
-var logRoutes = require("./Routes/Pass.Route");
-var mailRoutes = require("./Routes/Mail.Route");
-var dutyRoutes = require("./Routes/Duty.Route");
+//require routes here
 
-routes.use("/drug", drugRoutes);
-routes.use("/emp", empRoutes);
-routes.use("/issue", issueRoutes);
-routes.use("/payment", paymentRoutes);
-routes.use("/issueddrugs", issuedDrugsRoutes);
-routes.use("/log", logRoutes);
-routes.use("/mail", mailRoutes);
-routes.use("/duty", dutyRoutes);
 
 module.exports = routes;
