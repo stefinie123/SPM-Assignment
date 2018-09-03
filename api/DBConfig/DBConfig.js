35d190dfbb4c9cc7562e2c8cfa250d8db6dd1010
Spencer Fern <USpenSt@PEROOT.COM>
2018-09-03 10:48:59 +0530

    Task #1326 and task #1328 Endpoints implementation

--- api/DBConfig/DBConfig.js
+++ api/DBConfig/DBConfig.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
 
+//requiring schemas
+var StudentSchema = require('../Models/StudentSchema');
+
+//registering models
+mongoose.model('Student', StudentSchema);
+
 mongoose.connect('mongodb://localhost:27017/Internships', function (err) {
     if(err){
         console.log(err);
